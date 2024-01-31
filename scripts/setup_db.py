7ea05a3f770d5f4ceb7d9e2620d47eaa5f6f8325
Jakob Willforss <jakob.willforss@hotmail.com>
2024-01-31 14:05:04 +0100

    Minimal sqlite add and list

--- scripts/setup_db.py
+++ scripts/setup_db.py
@@ -6,3 +6,6 @@ cur = con.cursor()
 cur.execute("CREATE TABLE path(id, label, path)")
 
 cur.execute("INSERT INTO path VALUES ('id1', 'testpath', '/home/jakob')")
+
+# https://docs.python.org/3/library/sqlite3.html
+res = cur.execute("SELECT name FROM sqlite_master")
