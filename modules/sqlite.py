5beb2235b2ac1db6ea3a89f383bd4c8a3a08576a
Jakob Willforss <jakob.willforss@hotmail.com>
2024-02-01 13:38:27 +0100

    rm cont

--- modules/sqlite.py
+++ modules/sqlite.py
@@ -1,11 +1,13 @@
 import sqlite3
 
 
-def execute_db(db: str, command: str):
+def execute_db(db: str, command: str) -> int:
     con = sqlite3.connect(db)
     cur = con.cursor()
     cur.execute(command)
     con.commit()
+    row_count = cur.rowcount
+    return row_count
 
 
 def get_cursor(db: str):
